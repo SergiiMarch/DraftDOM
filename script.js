1520d25d5e4131341c933ba0e14bf075f503cf8a
Sergii Marchenko <marikmarchelo@gmail.com>
2024-02-23 14:46:05 +0200

    add task DOM

--- script.js
+++ script.js
@@ -191,3 +191,32 @@ function onHandlerInput(event) {
     input.classList.add("invalid");
   }
 }
+
+/*Обробка відправлення форми form.login-form повинна відбуватися 
+відповідно до події submit.
+Під час відправлення форми сторінка не повинна перезавантажуватися.
+Якщо у формі є незаповнені поля, 
+виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+Якщо користувач заповнив усі поля і відправив форму, 
+збери значення полів в об'єкт, 
+де ім'я поля буде ім'ям властивості, 
+а значення поля - значенням властивості. 
+Для доступу до елементів форми використовуй властивість elements.
+Виведи об'єкт із введеними даними 
+в консоль і очисти значення полів форми методом reset.*/
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onHandlerSubmitForm);
+
+function onHandlerSubmitForm(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    if ((name.password = " ")) {
+      alert("Заповніть коректно поля password");
+    } else if ((name.email = " ")) {
+      alert("Заповніть коректно поля email");
+    }
+  });
+}
